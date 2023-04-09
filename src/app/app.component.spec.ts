77ca3bfd9d9d0ee9614ac13613a57f6fc335ec88
Monika Dembek <monika.dembek@knowit.eu>
2023-04-09 19:33:11 +0200

    Initial setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend-app'`, () => {
+  it(`should have as title 'my-pet-app-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend-app');
+    expect(app.title).toEqual('my-pet-app-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('my-pet-app-frontend app is running!');
   });
 });
